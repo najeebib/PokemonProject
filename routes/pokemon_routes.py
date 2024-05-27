15a9ccaf78e563ed5e9036f61685dd9c670d3d00
Shqier <shqier@icloud.com>
2024-05-27 10:37:50 +0300

    updates on routes

--- routes/pokemon_routes.py
+++ routes/pokemon_routes.py
@@ -7,10 +7,47 @@ from data.database import get_db
 from sqlalchemy import text
 from models.trainer import Trainer
 from models.pokemon import Pokemon
+
+
 router = APIRouter()
 
-@router.get('/add_pokemon')
-def add_pokemon(pokemon: Pokemon, trainer: Trainer):
-    # Add a pokemon to a trainer
+@router.post("/pokemon/", response_model=Pokemon)
+def create_pokemon(pokemon: Pokemon, db: Session = Depends(get_db)):
+    db_pokemon = Pokemon(name=pokemon.name, type=pokemon.type, height=pokemon.height, weight=pokemon.weight)
+    db.add(db_pokemon)
+    db.commit()
+    db.refresh(db_pokemon)
+    return db_pokemon
 
-    pass
+
+@router.get("/pokemon/{pokemon_id}", response_model=Pokemon)
+def read_pokemon(pokemon_id: int, db: Session = Depends(get_db)):
+    db_pokemon = db.query(Pokemon).filter(Pokemon.id == pokemon_id).first()
+    if db_pokemon is None:
+        raise HTTPException(status_code=404, detail="Pokemon not found")
+    return db_pokemon
+
+
+@router.put("/pokemon/{pokemon_id}", response_model=Pokemon)
+def update_pokemon(pokemon_id: int, pokemon: Pokemon, db: Session = Depends(get_db)):
+    db_pokemon = db.query(Pokemon).filter(Pokemon.id == pokemon_id).first()
+    if db_pokemon is None:
+        raise HTTPException(status_code=404, detail="Pokemon not found")
+    for var, value in vars(pokemon).items():
+        setattr(db_pokemon, var, value) if value else None
+    db.commit()
+    db.refresh(db_pokemon)
+    return db_pokemon
+
+
+@router.delete("/pokemon/{pokemon_id}")
+def delete_pokemon(pokemon_id: int, db: Session = Depends(get_db)):
+    db_pokemon = db.query(Pokemon).filter(Pokemon.id == pokemon_id).first()
+    if db_pokemon is None:
+        raise HTTPException(status_code=404, detail="Pokemon not found")
+    db.delete(db_pokemon)
+    db.commit()
+    return {"message": "Pokemon deleted"}
+
+
+# TODO: 
