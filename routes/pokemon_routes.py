937141d84f4b9eaeda76cf8a7de2c6a51f13c4e9
najeebib <najeebibraheem1999@hotmail.com>
2024-05-27 08:39:08 +0300

    added routes

--- routes/pokemon_routes.py
+++ routes/pokemon_routes.py
@@ -7,10 +7,48 @@ from data.database import get_db
 from sqlalchemy import text
 from models.trainer import Trainer
 from models.pokemon import Pokemon
+import utils.sql_queries as fns
+
 router = APIRouter()
 
-@router.get('/add_pokemon')
-def add_pokemon(pokemon: Pokemon, trainer: Trainer):
-    # Add a pokemon to a trainer
+@router.post('/add_pokemon')
+def add_pokemon(pokemon: Pokemon, trainer: Trainer, db: Session = Depends(get_db)):
+    # Add a pokemon to the pokemons db
+
+    pass
+
+@router.get('/pokemonByType')
+def get_pokemon_by_type(type: str,db: Session = Depends(get_db)):
+    # get pokemon by type
+
+    pass
+
+@router.get('/pokemonByTrainer')
+def get_pokemon_by_trainer(trainer_name: str, db: Session = Depends(get_db)):
+    # get pokemon by trainer
+
+    pass
+
+@router.get('/trainersOfPokemon')
+def get_trainers(pokemon_name: str, db: Session = Depends(get_db)):
+    # get all trainers by pokemon
+
+    pass
+
+@router.delete('/deletePokemonFromTrainer')
+def delete_pokemon_from_trainer(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
+    # delete pokemon from a trainer
+
+    pass
+
+@router.post('/addPokemonToTrainer')
+def add_pokemon_from_trainer(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
+    # add pokemon to a trainer
+
+    pass
+
+@router.put('/evolve')
+def evolve(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
+    # evolve a trainers pokemon
 
     pass
