b6a44cef23d06be231bb2537967cca1f7387b1c6
najeebib <najeebibraheem1999@hotmail.com>
2024-05-27 13:19:13 +0300

    added more routes
    added add pokemon and add trainer routes
    added add pokenon and delete pokemon to trainer

--- routes/pokemon_routes.py
+++ routes/pokemon_routes.py
@@ -9,15 +9,31 @@ from models.trainer import Trainer
 from models.pokemon import Pokemon
 import utils.sql_queries as fns
 import utils.select_queries as select_fns
+import utils.delete_queries as delete_fns
 
 router = APIRouter()
 
 @router.post('/addPokemon')
-def add_pokemon(pokemon: Pokemon, trainer: Trainer, db: Session = Depends(get_db)):
-    # Add a pokemon to the pokemons db
-    
+def add_pokemon(pokemon: Pokemon, db: Session = Depends(get_db)):
+    if not fns.is_pokemon_in_table(db, pokemon.name):
+        fns.insert_into_pokemons_table(db,name=pokemon.name, height=pokemon.height, weight=pokemon.weight )
 
-    pass
+    if not fns.is_type_in_table(db, type=pokemon.type):
+        fns.insert_into_types_table(db, [pokemon.type])
+
+    pokemon_obj = select_fns.select_pokemon(db, pokemon.name)
+    pokemon_id = pokemon_obj.pokemon_id
+
+    type_obj = select_fns.select_type(db, pokemon.type)
+    type_id = type_obj.types_id
+
+    if not fns.is_in_pokemontype(db, pokemon_id, type_id):
+        fns.insert_into_PokemonTypes_table(db, pokemon_id, type_id)
+
+@router.post('/addTrainer')
+def add_pokemon(trainer: Trainer, db: Session = Depends(get_db)):
+    if not fns.is_trainer_in_table(db, trainer.name):
+        fns.insert_into_trainers_table(db, trainer.name, trainer.town)
 
 @router.get('/pokemonByType/{type}')
 def get_pokemon_by_type(type: str,db: Session = Depends(get_db)):
@@ -33,15 +49,29 @@ def get_trainers(pokemon_name: str, db: Session = Depends(get_db)):
 
 @router.delete('/deletePokemonFromTrainer')
 def delete_pokemon_from_trainer(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
-    # delete pokemon from a trainer
+    pokemon = select_fns.select_pokemon(db, pokemon_name)
+    trainer = select_fns.select_trainer(db, trainer_name)
 
-    pass
+    pokemon_id, trainer_id = pokemon.pokemon_id, trainer.trainer_id
+    if pokemon and trainer and fns.is_in_pokedex(db, pokemon_id, trainer_id):
+        delete_fns.delete_from_pokedex(db, pokemon_id, trainer_id)
+    else:
+        #todo
+        pass
 
 @router.post('/addPokemonToTrainer')
 def add_pokemon_from_trainer(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
-    # add pokemon to a trainer
+    pokemon = select_fns.select_pokemon(db, pokemon_name)
+    trainer = select_fns.select_trainer(db, trainer_name)
 
-    pass
+    if pokemon and trainer:
+        pokemon_id, trainer_id = pokemon.pokemon_id, trainer.trainer_id
+        if not fns.is_in_pokedex(db, pokemon_id, trainer_id):
+            fns.insert_into_Pokedex_table(db, pokemon_id, trainer_id)
+        else:
+            print("already in")
+
+
 
 @router.put('/evolve')
 def evolve(trainer_name: str, pokemon_name: str, db: Session = Depends(get_db)):
