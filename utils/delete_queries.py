52205609e4cb9fe30c9f7cf1ddb5a0b55904fb7f
Shqier <shqier@icloud.com>
2024-05-27 13:21:56 +0300

    delete pokemon

---
+++ utils/delete_queries.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+import sql_queries as fns
+
+
+def delete_from_pokedex(db: Session, trainer_id: int, pokemon_id: int):
+    if fns.is_in_pokedex(db, pokemon_id, trainer_id):
+        db.execute(text(f"DELETE FROM `pokedex` WHERE pokemon_id = {pokemon_id} AND trainer_id = {trainer_id}"))
+        db.commit()
+
+    else:
+        raise Exception("Pokemon not in pokedex")
